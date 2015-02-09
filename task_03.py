3a8e40b32e2e7b9cc234620440bc45596b8c2344
Lalani Werake <lalanikw@gmail.com>
2015-02-09 10:35:56 +0000

    task_03.py

--- task_03.py
+++ task_03.py
@@ -1,7 +1,10 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""A file to say hello."""
+
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
